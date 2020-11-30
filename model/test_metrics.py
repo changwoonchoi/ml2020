34e3725ba27c8d89cefee92c8a8f0e51f1070d11
ccw <zzzmaster@snu.ac.kr>
2020-11-30 16:53:28 +0900

    implement distance metrics

---
+++ model/test_metrics.py
@@ -0,0 +1,54 @@
+from utils import *
+import scipy.io.wavfile
+import numpy as np
+import glob
+import argparse
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--gen_dir')
+
+args = parser.parse_args()
+
+# read reference set (test set)
+ref_set_list = []
+ref_set_list_file = open('../data/test.txt', 'r')
+while True:
+    line = ref_set_list_file.readline()[:-1]
+    if not line:
+        break
+    ref_set_list.append(line)
+ref_set_list_file.close()
+
+ref_set = []
+for ref_set_instance in ref_set_list:
+    _, sample = scipy.io.wavfile.read(ref_set_instance)
+    audio = sample.T
+    ref_set.append(audio)
+ref_set = np.stack(ref_set, axis=0)
+
+# read generated set (inferred audios)
+gen_set_file_list = glob.glob(os.path.join(args.gen_dir, '*.wav'))
+gen_set = []
+for gen_set_instance in gen_set_file_list:
+    _, sample = scipy.io.wavfile.read(gen_set_instance)
+    audio = sample.T
+    gen_set.append(audio)
+gen_set = np.stack(gen_set, axis=0)
+
+# test std
+stds_1 = side_std(gen_set[0])
+stds_2 = side_std(gen_set[1])
+d = distance(stds_1, stds_2)
+
+# test distance
+ref_set = make_set('../data/test.txt', type='txt')
+gen_set = make_set(args.gen_dir, type='dir')
+dist_mat = dist_matrix(gen_set, ref_set)
+# test MMD
+MMD = mmd(dist_mat)
+# test cov
+COV = cov(dist_mat)
+# test k-NNA
+k_NNA = k_nna(dist_mat)
+# test JSD
--- model/utils.py
+++ model/utils.py
@@ -1,71 +1,136 @@
+import os
+from glob import glob
 import numpy as np
+import torch
 from pyemd import emd
 import matplotlib.pyplot as plt
+import pandas as pd
+import scipy.io.wavfile
+from scipy.stats import wasserstein_distance
+
+
+def make_set(dir, type='dir'):
+    """
+    type : 'dir', 'txt'
+    returns stacked npy file that contains whole wav file in directory
+    """
+    if type == 'dir':
+        wavfiles = glob(os.path.join(dir, '*.wav'))
+    elif type == 'txt':
+        wavfiles = []
+        txt_file = open(dir, 'r')
+        while True:
+            line = txt_file.readline()[:-1]
+            if not line:
+                break
+            wavfiles.append(line)
+        txt_file.close()
+    else:
+        raise ValueError
+    wave_set = []
+    for wave_instance in wavfiles:
+        _, sample = scipy.io.wavfile.read(wave_instance)
+        audio = sample.T / 2**15
+        wave_set.append(audio)
+    wave_set = np.stack(wave_set, axis=0)
+    return wave_set
+
+
+def side_std(audio):
+    """
+    audio : np array of size (N x 2)
+    output : normalized distribution of Side sound for input audio
+    """
+    t_index, stds = std_over_time(audio, sr=44100, hop_size=1, threshold=0.125, display=False, continue_plot=False)
+    return stds
+
+
+def normalize_std(std):
+    cum = 0
+    for i in range(std.shape[0]):
+        cum += std[i]
+    std *= (10000. / cum)
+    return std
+
 
 def distance(std1, std2):
-    '''
+    """
     std1 : distribution of std(S) for audio1
     std2 : distribution of std(S) for audio2
     output : EMD(earth mover's distance) of two distribution
-    '''
+    """
     # normalize std1, std2 to convert into probabilistic distribution function
-
+    std1_norm = normalize_std(std1)
+    std2_norm = normalize_std(std2)
     # calculate EMD between two pdfs
+    d = wasserstein_distance(std1_norm, std2_norm)
+    return d
 
-    raise NotImplementedError
 
-def side_std(audio):
-    '''
-    audio : np array of size (N x 2)
-    output : normalized distribution of Side sound for input audio
-    '''
-    raise NotImplementedError
+def dist_matrix(set1, set2):
+    """
+    set_1 : normal set of inferred audios
+    set_2 : normal set of reference audios (have same size with set_1)
+    output :  distance matrix between two audio sets
+    """
+    g = set1.shape[0]
+    r = set2.shape[0]
+    gen_ref_set = torch.cat((set1, set2), dim=0).view((g + r, 1, set1.shape[1], -1))
+    dist_mat = torch.zeros((g + r, g + r))
+    for i in range(g + r):
+        for j in range(i + 1, g + r):
+            dist_mat[i][j] = distance(gen_ref_set[i], gen_ref_set[j])
 
-def cov(set_1, set_2):
-    '''
+    return g, r, dist_mat
+
+
+def cov(dist_mat):
+    """
     set_1 : normal set of inferred audios
     set_2 : normal set of reference audios (have same size with set_1)
     output : calculated Coverage between inferred set and reference set
-    '''
+    """
     raise NotImplementedError
 
-def mmd(set_1, set_2):
-    '''
+
+def mmd(dist_mat):
+    """
     set_1 : normal set of inferred audios
     set_2 : normal set of reference audios (have same size with set_1)
     output : calculated MMD(Minimum Matching Distance) between inferred set and reference set
-    '''
+    """
     raise NotImplementedError
 
-def k_NNA(k, set_1, set_2):
-    '''
+
+def k_nna(k, dist_mat):
+    """
     k : parameter for k-NNA(k-nearest neighbor accuracy)
     set_1 : normal set of inferred audios
     set_2 : normal set of reference audios (have same size with set_1)
     output : calculated k-NNA
-    '''
+    """
     raise NotImplementedError
 
+
 def jsd(set_1, set_2):
-    '''
+    """
     set_1 : normal set of inferred audios
     set_2 : normal set of reference audios (have same size with set_1)
     output : JSD(Jensen-Shannon Divergence) between two sets
-    '''
+    """
+
 
 def classify(audio):
-    '''
+    """
     returns the class of stereo audio
     {NNN: 0, NNW: 1, NWN: 2, ..., WWW: 7}
-    '''
+    """
     raise NotImplementedError
 
 
-### function std_over_time
-
-def std_over_time(audio, sr=44100, win_size = 1764, hop_size = None,
-                  threshold = 0.125, display=False, continue_plot=False):
-    '''
+def std_over_time(audio, sr=44100, win_size=1764, hop_size=None,
+                  threshold=0.125, display=False, continue_plot=False):
+    """
     ## inputs
     audio: np array of size 2 x 44100*0.4
     win_size: (default)40ms
@@ -76,7 +141,7 @@ def std_over_time(audio, sr=44100, win_size = 1764, hop_size = None,
 
     ## outputs
     t_index, stds
-    '''
+    """
     if hop_size == None:
         hop_size = int(win_size / 4)
     theta = np.pi / 4
@@ -87,8 +152,9 @@ def std_over_time(audio, sr=44100, win_size = 1764, hop_size = None,
     t_index = np.arange(int(win_size / 2), int(len(points) - win_size / 2), hop_size) / sr
     for t in np.arange(0, int(len(points) - win_size), hop_size):
         stds.append(np.std(points[t:t + win_size]))
-    if display == True:
-        if continue_plot == False:
+    stds = np.array(stds)
+    if display:
+        if not continue_plot:
             plt.figure(figsize=(15, 8))
         plt.plot(t_index, stds, linewidth=1)
         plt.axhline(threshold, color='r')
@@ -97,17 +163,16 @@ def std_over_time(audio, sr=44100, win_size = 1764, hop_size = None,
         plt.title('std moving window')
         plt.xlabel('time(s)')
         plt.ylabel('standard deviation')
-        if continue_plot == False:
+        if not continue_plot:
             plt.show()
     return t_index, stds
 
-### function find_label
 
 def find_label(file_path):
     '''
     file_path : single .wav file path or file name as string
     output : label('N' or 'W') of .wav(ex: ['N','W','W'])
     '''
-    label = pd.read_csv('../data/label.csv',  index_col=0)
+    label = pd.read_csv('../data/label.csv', index_col=0)
     file_name = file_path.split('/')[-1]
-    return label.loc[file_name,:].values
+    return label.loc[file_name, :].values
