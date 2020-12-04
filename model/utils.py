2f850244798403c2ed5b1ef7621d47f5524353ae
sangwoo81 <48647491+sangwoo81@users.noreply.github.com>
2020-12-05 05:37:16 +0900

    Update utils.py
    add wave_set2score_set(), score_over_time() which is expanded for metric in [‘std’, ‘quantile’, ‘pca ratio’, ‘pca angle’, ‘lr angle’, ‘correlation’]

--- model/utils.py
+++ model/utils.py
@@ -6,6 +6,8 @@ import pandas as pd
 import scipy.io.wavfile
 from scipy.stats import wasserstein_distance
 
+from sklearn.decomposition import PCA
+from sklearn.linear_model import LinearRegression
 
 def make_set(dir, type='dir'):
     """
@@ -55,7 +57,24 @@ def side_std(audio):
     output : normalized distribution of Side sound for input audio
     """
     t_index, stds = std_over_time(audio, sr=44100, hop_size=1, threshold=0.125, display=False, continue_plot=False)
+
     return stds
+
+def wave_set2score_set(wave_set, metric='std'):
+    """
+    convert wave set to set of scores over time
+    wave_set : (N x 2 x -1)
+    metric : [‘std’, ‘quantile’, ‘pca ratio’, ‘pca angle’, ‘lr angle’, ‘correlation’]
+    output : (N x -1)
+    """
+    score_set = []
+    for i in range(wave_set.shape[0]):
+        wave = wave_set[i]
+        _, score = score_over_time(wave, metric, sr=44100, hop_size=None, display=False, continue_plot=False)
+        score_set.append(score)
+    score_set = np.stack(score_set, axis=0)
+    return score_set
+
 
 
 def normalize_std(std):
@@ -232,6 +251,121 @@ def std_over_time(audio, sr=44100, win_size=1764, hop_size=None,
         if not continue_plot:
             plt.show()
     return t_index, stds
+
+
+def score_over_time(audio, metric = 'std', sr=44100, win_size=1764, hop_size=None,
+                  display=False, continue_plot=False):
+    """
+    ## inputs
+    audio: np array of size 2 x 44100*0.4
+    metric: [‘std’, ‘quantile’, ‘pca ratio’, ‘pca angle’, ‘lr angle’, ‘correlation’]
+    win_size: (default)40ms
+    hop_size: (default)0.25*win_size
+    display: True for show figure
+    continue_plot: True for many plots in one figure
+
+    ## outputs
+    t_index, scores
+    """
+    if hop_size == None:
+        hop_size = int(win_size / 4)
+    l = audio.shape[1]
+    t_index = np.arange(int(win_size / 2), int(l - win_size / 2), hop_size) / sr
+    scores = []
+    
+    if metric == 'std':
+        theta = np.pi / 4
+        trans = np.array([[np.cos(theta), -np.sin(theta)],
+                          [np.sin(theta), np.cos(theta)]])
+        points = np.dot(trans, audio)[0]
+        for t in np.arange(0, int(l - win_size), hop_size):
+            scores.append(np.std(points[t:t + win_size]))
+            
+    elif metric == 'quantile':
+        theta = np.pi / 4
+        trans = np.array([[np.cos(theta), -np.sin(theta)],
+                          [np.sin(theta), np.cos(theta)]])
+        points = np.dot(trans, audio)[0]
+        for t in np.arange(0, int(l - win_size), hop_size):
+            scores.append(np.diff(np.quantile(points[t:t + win_size],[0.05,0.95]))) 
+            
+    elif metric == 'pca ratio':
+        ratio = []        
+        for t in np.arange(0, int(l - win_size), hop_size):
+            L = audio[0,t:t + win_size]+1e-5*np.random.randn(win_size) # small perturbation for aviod inf
+            R = audio[1,t:t + win_size]+1e-5*np.random.randn(win_size)
+            pca = PCA(n_components=2)
+            pca.fit(np.vstack((L,R)).T)
+            if pca.components_[0,0]*pca.components_[0,1]>0:
+                r = pca.explained_variance_[0]/pca.explained_variance_[1]
+            else:
+                r = pca.explained_variance_[1]/pca.explained_variance_[0]
+    #             print('reverse')
+            ratio = np.append(ratio, r)    
+        scores = np.log(ratio)   
+        
+    elif metric == 'pca angle':
+        slope = []        
+        for t in np.arange(0, int(l - win_size), hop_size):
+            L = audio[0,t:t + win_size]
+            R = audio[1,t:t + win_size]
+            pca = PCA(n_components=2)
+            pca.fit(np.vstack((L,R)).T)
+            a = pca.components_[0,1]/pca.components_[0,0]
+            a = np.arctan(a)
+            if (a >= -np.pi/2) and (a <= -np.pi/4): # set range of slope as [-90,90]
+                a = a+np.pi
+            a = a*180/np.pi-45 # set y=x as degree of zero
+            slope = np.append(slope, a) 
+        scores = slope
+        
+    elif metric == 'lr angle':
+        slope = []
+        for t in np.arange(0, int(l - win_size), hop_size):
+            L = audio[0,t:t + win_size]
+            R = audio[1,t:t + win_size]
+            reg = LinearRegression(fit_intercept=False).fit(L.reshape(-1,1), R)
+            a = reg.coef_[0]
+            a = np.arctan(a)
+            if (a >= -np.pi/2) and (a <= -np.pi/4): # set range of slope as [-90,90]
+                a = a+np.pi
+            a = a*180/np.pi-45 # set y=x as degree of zero
+            slope = np.append(slope, a) 
+        scores = slope
+
+    elif metric == 'correlation':
+        for t in np.arange(0, int(l - win_size), hop_size):
+            L = audio[0,t:t + win_size]
+            R = audio[1,t:t + win_size]
+            c = pd.DataFrame(np.vstack((L,R)).T).corr().iloc[0,1]
+            scores  = np.append(scores , c)
+            
+    if display:
+        if not continue_plot:
+            plt.figure(figsize=(15, 8))
+        plt.plot(t_index, scores, linewidth=1)
+        plt.xlim([0, 0.4])
+        if metric =='std':
+            plt.ylim([0, 0.5])
+            plt.axhline(0.125,color='r')
+        elif metric =='pca ratio':
+            plt.ylim([-5, 10])
+            plt.axhline(0,color='r')
+        elif metric =='pca angle' or metric =='lr angle':
+            plt.ylim([-90, 90])
+            plt.axhline(0,color='r')
+        elif metric =='quantile':
+            plt.ylim([0, 1])
+            plt.axhline(0.25,color='r')
+        elif metric =='correlation':
+            plt.ylim([-1, 1])
+            plt.axhline(0.0,color='r')
+        plt.title(f'{metric} moving window')
+        plt.xlabel('time(s)')
+        plt.ylabel(f'{metric}')
+        if not continue_plot:
+            plt.show()
+    return t_index, scores
 
 
 def find_label(file_path):
